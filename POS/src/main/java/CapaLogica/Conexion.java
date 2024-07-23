8a1e9a014c829e417f7825f03dd4d1c49cee39ed
Josh2004LC <mlsjos5577@gmail.com>
2024-07-22 19:17:51 -0600

    Conexion creada

--- POS/src/main/java/CapaLogica/Conexion.java
+++ POS/src/main/java/CapaLogica/Conexion.java
@@ -4,10 +4,29 @@
  */
 package CapaLogica;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Josh
  */
 public class Conexion {
-    
+    static Connection conn = null;
+    public static Connection conectar(){
+        String url="jdbc:oracle:thin:@localhost:1521:xe";
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Error de sql "+ e.getMessage());
+        }
+        
+        try {
+            conn =DriverManager.getConnection(url,"P4Proyec","Dev2024");
+        } catch (SQLException e) {
+             System.out.println("Error de conexion del driver "+ e.getMessage());
+        }
+        return conn;
+    }
 }
