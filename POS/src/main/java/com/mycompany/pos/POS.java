7ca470ea117f53d5ef7d034c895a530c759c4479
Josh2004LC <mlsjos5577@gmail.com>
2024-07-22 19:05:19 -0600

    Add JDBC

---
+++ POS/src/main/java/com/mycompany/pos/POS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pos;
+
+/**
+ *
+ * @author Josh
+ */
+public class POS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
